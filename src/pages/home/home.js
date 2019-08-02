c5ce13dbcdfd171f2431463eb9cabc9b6ad6059a
crushton <cameron.rushton@solace.com>
2019-08-02 18:02:10 -0400

    removed commented code

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,32 +3,8 @@ import { Router } from "aurelia-router"
 @inject(Router)
 export class Home {
 
-    constructor(router) {
-        // this.router = router;
-        // this.router.routes.forEach((route, index) => {
-        //     if (route.name === "home") {
-        //         route.char = "H";
-        //     } else {
-        //         route.char = index;
-        //     }
-        // })
-        // this.screenWidth = screen.width;
-        // this.showNavOptions = false;
+    constructor() {
+
     }
-
-    // attached() {
-    //     window.addEventListener("scroll", event => this.handleScrollEvent(event));
-    // }
-
-    // detached() {
-    //     window.removeEventListener("scroll", event => this.handleScrollEvent(event))
-    // }
-
-    // handleScrollEvent(event) {
-    //     this.scrollPosition = event.currentTarget.scrollY;
-    //     if (this.scrollPosition > 90) {
-    //         this.showNavOptions = false;
-    //     }
-    // }
 
 }
