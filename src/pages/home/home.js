5203a0716aa1cb18c655cc10dafd6d26da46c90d
CameronRushton <cameron.rushton@rogers.com>
2019-08-03 00:02:47 -0400

    merged master

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,7 +3,67 @@ import { Router } from "aurelia-router"
 @inject(Router)
 export class Home {
 
+<<<<<<< Updated upstream
     constructor() {
+=======
+    constructor(router) {
+        this.router = router;
+        this.router.routes.forEach((route, index) => {
+            if (route.name === "home") {
+                route.char = "H";
+            } else {
+                route.char = index;
+            }
+        })
+
+        this.levels = [
+            {
+                number: 1,
+                title: "Beginner",
+                description: "I'm new to anime",
+                color: "green",
+                image: "assets/images/dragon.svg" //Rabbit
+            },
+            {
+                number: 2,
+                title: "Novice",
+                description: "I've seen a few",
+                color: "blue",
+                image: "assets/images/dragon.svg" //cat
+            },
+            {
+                number: 3,
+                title: "Intermediate",
+                description: "I've seen dozens",
+                color: "violet",
+                image: "assets/images/dragon.svg" //Wolf
+            },
+            {
+                number: 4,
+                title: "Advanced",
+                description: "I've seen hundreds",
+                color: "orange",
+                image: "assets/images/dragon.svg" //Griffin
+            },
+            {
+                number: 5,
+                title: "Expert",
+                description: "I've seen it all",
+                color: "red",
+                image: "assets/images/dragon.svg" //Dragon
+            }
+        ]
+
+    }
+
+    attached() {
+        window.addEventListener("scroll", event => this.handleScrollEvent(event));
+    }
+
+    detached() {
+        window.removeEventListener("scroll", event => this.handleScrollEvent(event))
+    }
+>>>>>>> Stashed changes
 
     }
 
