7a35c24b6e0bbcbc429604a55292d635669ebdbd
CameronRushton <cameron.rushton@rogers.com>
2019-08-06 22:47:24 -0400

    fixed home screen a little

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,44 +1,55 @@
+import { inject, observable } from 'aurelia-framework';
+import { Router } from "aurelia-router"
+
+@inject(Router)
 export class Home {
 
-    constructor() {
+    constructor(router) {
+        this.router = router;
+    }
 
+    attached() {
         this.levels = [
             {
                 number: 1,
                 title: "Beginner",
                 description: "I'm new to anime",
                 color: "green",
-                image: "assets/images/dragon-teal.png" //Rabbit
+                image: "assets/images/dragon-teal.png", //Rabbit
+                route: this.router.routes[1].route
             },
             {
                 number: 2,
                 title: "Novice",
                 description: "I've seen a few",
                 color: "blue",
-                image: "assets/images/dragon-teal.png" //cat
+                image: "assets/images/dragon-teal.png", //cat
+                route: this.router.routes[2].route
             },
             {
                 number: 3,
                 title: "Intermediate",
                 description: "I've seen dozens",
                 color: "violet",
-                image: "assets/images/dragon-teal.png" //Wolf
+                image: "assets/images/dragon-teal.png", //Wolf
+                route: this.router.routes[3].route
             },
             {
                 number: 4,
                 title: "Advanced",
                 description: "I've seen hundreds",
                 color: "orange",
-                image: "assets/images/dragon-teal.png" //Griffin
+                image: "assets/images/dragon-teal.png", //Griffin
+                route: this.router.routes[4].route
             },
             {
                 number: 5,
                 title: "Expert",
                 description: "I've seen it all",
                 color: "red",
-                image: "assets/images/dragon-teal.png" //Dragon
+                image: "assets/images/dragon-teal.png", //Dragon
+                route: this.router.routes[5].route
             }
         ]
-
     }
 }
