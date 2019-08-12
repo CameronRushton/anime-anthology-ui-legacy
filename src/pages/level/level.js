69f746bc41563fa6618c0ea0f63e20cba59c67f4
CameronRushton <cameron.rushton@rogers.com>
2019-08-11 21:57:00 -0400

    Heros

--- src/pages/level/level.js
+++ src/pages/level/level.js
@@ -54,12 +54,20 @@ export class Level {
             }
         })
 
-        let descriptions = ["New to anime? Start with these. There will be something you like in here. We're just skimming the surface. Watch at least 4 of these to continue to level two.",
+        let descriptions = ["New to anime? Start with these. There will be something you like in here. We're just scratching the surface. Watch at least 4 of these to continue to level two.",
             "Other great anime that didn't quite make the cut into the top. At this stage, you think anime is kinda cool and want to see what more it has to offer. Watch at least 5 of these to continue to level three.",
             "These are good and some are great anime, but for various reasons, didn't make it to the top. At this point, you should be a certified weeb and are losing your grip on life because of how great anime is.Watch at least 10 of these to continue to level four.",
             "This level has anime that I wouldn't recommend for beginners. It may be complex, not define the anime genre well and turn people off or are just not good to watch. Since you should watch shows in this level after you're done with the other three levels, you may find some shows here to be either bad, or extremely interesting because of how complex or different the show is. Quality is very hit or miss. This is near the end game. Watch at least a total of 70 shows to continue to level five.",
             "The end game. You must have watched a lot of anime to not be put off by some of the stuff in here. Some of it is bad, some of it is really good. Assume most are bad."]
 
         this.level.description = descriptions[this.level.number-1]
+        this.level.hero = `assets/images/level-${this.level.number}-hero.jpg`
+        switch(this.level.number) {
+            case 1: this.level.heroPosition = "0% 25%"; break;
+            case 2: this.level.heroPosition = "0% 50%"; break;
+            case 3: this.level.heroPosition = "0% 75%"; break;
+            case 4: this.level.heroPosition = "0% 60%"; break;
+            case 5: this.level.heroPosition = "0% 90%"; break;
+        }
     }
 }
